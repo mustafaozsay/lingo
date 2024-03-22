121a3fe5c4ae53d4b07125c60fc20987c71fafa9
mustafaozsay <mustafaozsay@gmail.com>
2024-03-22 16:33:17 +0300

    seed script added
    db scripts added and changed some  static data

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,14 +1,21 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUSerProgress } from "@/db/queries";
 import List from "./list";
 
 export default async function CoursesPage() {
-  const courses = await getCourses();
+  const coursesData = getCourses();
+
+  const userProgressData = getUSerProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
 
-      <List courses={courses} activeCourseId={1} />
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 }
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,21 +2,32 @@ import FeedWrapper from "@/components/feed-wrapper";
 import StickyWrapper from "@/components/stick-wrapper";
 import Header from "./header";
 import UserProgress from "@/components/user-progress";
+import { userProgress } from "@/db/schema";
+import { getUSerProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-export default function LearnPage() {
+export default async function LearnPage() {
+  const userProgresData = getUSerProgress();
+
+  const [userProgress] = await Promise.all([userProgresData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "./es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
 
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
